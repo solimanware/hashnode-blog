94d0f988a95b093803dc84a5fc556091f6426d12
Osama Soliman <solimanware@gmail.com>
2024-04-12 16:14:13 +0200

    enhance footer and header

--- packages/blog-starter-kit/themes/hashnode/components/publication-footer.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/publication-footer.tsx
@@ -28,56 +28,13 @@ function PublicationFooter(props: any) {
           ></div>
         </section>
       )}
-      <div className="blog-footer-credits flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <div className="mb-12 flex flex-col flex-wrap items-center">
           <p className="mb-2 text-slate-600 dark:text-slate-300">
-            &copy;{new Date().getFullYear()} {title || `${authorName}'s Blog`}
+           All Rights Reserved, &copy;{new Date().getFullYear()} {title || `${authorName}'s Blog`}
           </p>
-          <div className="flex flex-row items-center text-slate-600 dark:text-slate-300">
-            <a href="https://hashnode.com/privacy?source=blog-footer" className="mx-2 underline">
-              Privacy policy
-            </a>
-            <span className="font-extrabold text-black opacity-20 dark:text-white">&middot;</span>
-            <a className="mx-2 underline" href="https://hashnode.com/terms?source=blog-footer">
-              Terms
-            </a>
-          </div>
+          <p>Made with ❤️ by <a href="https://x.com/solimanware" className='mb-2 text-slate-600 dark:text-slate-300'>Soliman</a></p>
         </div>
-        {disableFooterBranding ? (
-          <>
-            {logo && (
-              <div className="flex flex-col items-center">
-                <Link href="/" className="relative block h-10 w-40">
-                  <Image
-                    layout="fill"
-                    alt={title || `${authorName}'s ${isTeam ? 'team' : ''} blog`}
-                    src={resizeImage(logo, { w: 1000, h: 250, c: 'thumb' })}
-                  />
-                </Link>
-              </div>
-            )}
-          </>
-        ) : (
-          <div className="flex flex-col items-center">
-            <Link
-              aria-label="Publish with Hashnode"
-              className="mb-4 flex flex-row items-center rounded-lg border border-slate-300 bg-white p-3 font-heading font-bold tracking-wide text-slate-600 transition-colors duration-75 hover:border-slate-400 hover:text-slate-900 dark:border-slate-800 dark:bg-black dark:text-slate-300 dark:hover:border-slate-500 dark:hover:text-white"
-              href="https://hashnode.com/onboard?unlock-blog=true&source=blog-footer"
-            >
-              <span className="mr-2 block text-blue-600">
-                <HashnodeLogoIconV2 className="h-6 w-6 fill-current" />
-              </span>
-              <span>Publish with Hashnode</span>
-            </Link>
-            <p className="text-sm text-slate-600 dark:text-slate-300">
-              Powered by{' '}
-              <a aria-label="Hashnode" href="https://hashnode.com?source=blog-footer" className="underline">
-                Hashnode
-              </a>{' '}
-              - Home for tech writers and readers
-            </p>
-          </div>
-        )}
       </div>
     </footer>
   );
